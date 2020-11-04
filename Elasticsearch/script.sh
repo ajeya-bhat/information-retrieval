527967a986f68fe035dd1a634668e7155b6333ad
harishpb26 <harishpb.26@gmail.com>
2020-11-04 11:19:23 +0530

    Insert data into Elasticsearch

---
+++ Elasticsearch/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# run ES
+# ../../elasticsearch-7.9.3/bin/elasticsearch
+
+# make sure ES is up and running
+curl -X GET 'http://localhost:9200'
+echo '\n'
+sleep 1
+
+# insert data from csv
+# ../../logstash-7.9.3/bin/logstash -f ./logstash-csv.conf
+
+# list all documents in an index
+curl -X GET 'http://localhost:9200/environmental-news/_search?pretty=true&q=*:*&size=12'
+echo '\n'
