45e4d71d70aed45e6c9e5d66866a562b33a03329
ajeya-bhat <ajeyabs@gmail.com>
2020-11-07 20:57:05 +0530

    formatting of output

--- query.py
+++ query.py
@@ -2,6 +2,7 @@ import pickle
 from collections import defaultdict
 from config import config_params
 import indexes as index
+import json
 
 
 def preprocess_query(query):
@@ -41,11 +42,56 @@ if config_params["index"] == 1:
 elif config_params["index"] == 2:
   index=index.BooleanQuery(data_dict['rowterms'])
 
-query = "brazil's governmen* is defending its plan to build dozens of huge hydro-electric dams"
+query = "brazil's government is defending its plan to build dozens of huge hydro-electric dams"
 query, channel, show = preprocess_query(query)
 docs = index.query(query)
 docs = postprocess_query(docs, channel, show)
 
-for docid in docs[:10]:
-  print(data_dict['rowsnip'][docid])
-  print()
+json_res={}
+if config_params['index']==1:
+  json_res['index']="vector space model(tf idf)"
+elif config_params['index']==2:
+  json_res['index']="boolean query"
+
+if config_params['stopword_removal']==1:
+  json_res['stopword_removal']=True
+else:
+  json_res['stopword_removal']=False
+
+if config_params['preprocess_type']==1:
+  json_res['preprocessing']="stemming"
+elif config_params['preprocess_type']==2:
+  json_res['preprocessing']="lemmatization"
+elif config_params['preprocess_type']==3:
+  json_res['preprocessing']="none"
+
+if config_params['spell_check']==1:
+  json_res['spell_check']=True
+else:
+  json_res['spell_check']=False
+
+if config_params['tf_scheme']==1:
+  json_res['tf_scheme']="Normal TF"
+elif config_params['tf_scheme']==2:
+  json_res['tf_scheme']="1+log(tf)"
+elif config_params['tf_scheme']==3:
+  json_res['tf_scheme']="log(1+tf)"
+
+
+if len(docs)>100:
+  json_res['number_of_hits']='100+'
+else:
+  json_res['number_of_hits']=len(docs)
+if(len(docs)>100):
+  docs=docs[:100]
+json_res['hits']=[]
+for docid in docs:
+  resdict={}
+  resdict['id']=data_dict['rowdict'][docid][0]
+  resdict['document_name']=data_dict['rowdict'][docid][1]
+  resdict['station']=data_dict['rowdict'][docid][2]
+  resdict['show']=data_dict['rowdict'][docid][3]
+  resdict['snippet']=data_dict['rowsnip'][docid]
+  json_res['hits'].append(resdict)
+
+print(json.dumps(json_res,indent=1))
