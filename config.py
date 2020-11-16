2efa77fccbcf26970c79ae1c01f5b2175bdd034d
Pradyumna YM <pradyu99914@gmail.com>
2020-11-16 18:50:07 +0530

    added description of config params

--- config.py
+++ config.py
@@ -15,3 +15,10 @@ config_params={
 #stop-word removal : 1 is to do stop-word removal , 0 is to not do stop word removal
 #index : 1 is for tf-idf, 2 is for boolean query, 3 for Positional Index
 #tf scheme : 1 is directly taking tf, 2 is 1+log(tf), 3 is log(1+tf)
+#threshold_score : The minimum normalized threshold score for a document. 
+                  #uses top 10 percentile if no document crosses this threshold
+#spell_check: A flag to toggle spelling correction using edit distance.
+#es_index : name for the elasticsearch index.
+#result_size : the maximum number of documents a search shoukld return.
+#es_preprocess : A flag to toggle preprocessing for elastic search.
+#es_host and es_port : The hostname and port number for the elasticsearch server.
